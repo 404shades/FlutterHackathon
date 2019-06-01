4d4b19de19c34a42fe5977e2de3bc6ee33330523
Rohan Malik <malikmalik29@outlook.com>
2019-06-01 11:32:27 +0530

    Initial Commit

---
+++ android/app/src/main/java/flutterkack/com/flutter_hackathon/MainActivity.java
@@ -0,0 +1,13 @@
+package flutterkack.com.flutter_hackathon;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
